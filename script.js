5c31bd9a80bbcbb6967ea16da09123ca20f481a2
Khaled Ajaj <khaledajaj2026@gmail.com>
2023-07-03 19:42:02 -0700

    successfuly implemented localStorage for row counter, keeps track of current row this computer is currently on.

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 // keeps track of number of rows, adds num to class of new <tr>.
-let rowNum = 0;
+// stores current user's rows in local machine storage.
+let rowNum = localStorage.getItem('row-num') ? 
+JSON.parse(localStorage.getItem('row-num')) : 0;
 function addRow() {
     // set inputs as variables for later initialization & to check if all entered.
-    const qVal = document.getElementsByName("question")[0].value;
-    const sVal = document.getElementsByName("source")[0].value;
-    const cVal = document.getElementsByName("categories")[0].value;
-    const dVal = document.getElementsByName("date")[0].value;
-    const tVal = document.getElementsByName("time")[0].value;
+    const qVal = document.getElementsByName('question')[0].value;
+    const sVal = document.getElementsByName('source')[0].value;
+    const cVal = document.getElementsByName('categories')[0].value;
+    const dVal = document.getElementsByName('date')[0].value;
+    const tVal = document.getElementsByName('time')[0].value;
     
     if(qVal && sVal && cVal && dVal && tVal) {
         // increment rowNum by 1.
         rowNum++;
+        localStorage.setItem('row-num', JSON.stringify(rowNum))
         // get table element.
-        const dsaTable = document.getElementById("table");
+        const dsaTable = document.getElementById('table');
         // initialize new row and insert input values.
         dsaTable.innerHTML += `
-        <tr class="row-${rowNum}">
+        <tr class='row-${rowNum}'>
             <td>Problem #${rowNum}</td>
             <td>${qVal}</td>
             <td>${sVal}</td>
@@ -23,27 +26,25 @@ function addRow() {
             <td>${dVal}</td>
             <td>${tVal}</td>
         <tr>`;
-        localStorage.setItem(JSON.stringify(rowNum), JSON.stringify(dsaTable));
-        console.log(JSON.parse(localStorage.getItem("1")));
         // clear input fields for new entry.
         // using variables like 'qVal' does not work, reference error.
-        document.getElementsByName("question")[0].value = "";
-        document.getElementsByName("source")[0].value = "";
-        document.getElementsByName("categories")[0].value = "";
-        document.getElementsByName("date")[0].value = "";
-        document.getElementsByName("time")[0].value = "";
+        document.getElementsByName('question')[0].value = '';
+        document.getElementsByName('source')[0].value = '';
+        document.getElementsByName('categories')[0].value = '';
+        document.getElementsByName('date')[0].value = '';
+        document.getElementsByName('time')[0].value = '';
     } else {
-        alert("One or more fields are empty. Fill out form before pressing \"Enter\".");
+        alert('One or more fields are empty. Fill out form before pressing \"Enter\".');
     }
 }
 function deleteRow() {
     // get input row to delete as variable.
-    const selectedRow = document.getElementsByName("deleteRow")[0].value;
+    const selectedRow = document.getElementsByName('deleteRow')[0].value;
     // if-else statement checking validity of input row.
     if (!selectedRow) {
-        alert("No row specified for deletion.");
+        alert('No row specified for deletion.');
     } else if(rowNum === 0){
-        alert("No rows to delete.");
+        alert('No rows to delete.');
     } else if (selectedRow > 0 && selectedRow <= rowNum) {
         // row input via jQuery.
         $(`.row-${selectedRow}`).remove();
@@ -52,5 +53,5 @@ function deleteRow() {
     } else {
         alert(`Invalid row number. Please enter a number between ${1} and ${rowNum}`);
     }
-    document.getElementsByName("deleteRow")[0].value = "";
+    document.getElementsByName('deleteRow')[0].value = '';
 }
